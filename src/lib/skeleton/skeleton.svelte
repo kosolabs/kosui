a2eb3595183b245f0c0e27b36ed28af4202c9c07
Shad Sharma <shadanan@gmail.com>
2025-08-03 16:48:07 -0700

    Adds skeleton component and updates branding (#14)

---
+++ src/lib/skeleton/skeleton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import { twMerge } from "tailwind-merge";
+  import { Box, type BoxProps } from "../box";
+
+  export type SkeletonProps = { children?: Snippet } & BoxProps;
+  let {
+    children,
+    class: className,
+    backdrop = true,
+    shape = "rounded",
+    ...restProps
+  }: SkeletonProps = $props();
+</script>
+
+<Box
+  class={twMerge("animate-pulse", className)}
+  {backdrop}
+  {shape}
+  {...restProps}
+>
+  {@render children?.()}
+</Box>
---
+++ src/routes/skeleton/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Skeleton } from "$lib/skeleton";
+</script>
+
+<Skeleton class="h-9 p-4" />
+<Skeleton shape="circle" class="size-9" />
+<Skeleton class="h-20 p-4" />
+<Skeleton shape="circle" class="ml-auto size-9" />
